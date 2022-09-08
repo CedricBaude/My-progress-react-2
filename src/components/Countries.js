904bf5a513aa55159d9bc06c4f5cdd0539a93de3
CedricBaude <baude.cedric.perso@gmail.com>
2022-09-08 15:09:05 +0200

    filter nav

--- src/components/Countries.js
+++ src/components/Countries.js
@@ -6,6 +6,8 @@ import Card from "./Card";
 const Countries = () => {
     const [data, setData] = useState([]);
     const [rangeValue, setRangeValue] = useState(36);
+    const [selectedRadio, setSelectedRadio] = useState("");
+    const radios = ["Africa", "America", "Asia", "Europe", "Oceania"];
 
     // Le useEffect se joue lorsque le composant est monté
     useEffect(() => {
@@ -15,14 +17,22 @@ const Countries = () => {
     return (
         <div className='countries'>
             <ul className="radio-container">
-                <input type="range" min="1" ma="250" defaultValue={rangeValue}  onChange={(e) => console.log(e)}/>
+                <input type="range" min="1" ma="250" defaultValue={rangeValue} onChange={(e) => setRangeValue(e.target.value)} />
+                {radios.map((continent) => (
+                    <li>
+                        <input type="radio" id={continent} name="continentRadio" onChange={(e) => setSelectedRadio(e.target.id)} />
+                        <label htmlFor={continent}>{continent}</label>
+                    </li>
+                ))}
             </ul>
             <ul>
                 {data
-                .slice(0, rangeValue)
-                .map((country, index) => (
-                    <Card key={index} country={country} />
-                ))}
+                    .filter((country) => country.continents[0].includes(selectedRadio))
+                    .sort((a,b) => b.population - a.population)
+                    .slice(0, rangeValue)
+                    .map((country, index) => (
+                        <Card key={index} country={country} />
+                    ))}
             </ul>
         </div>
     );
